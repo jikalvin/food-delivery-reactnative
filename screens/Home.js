2e8a99f9489464a5e6cdc67044bd298609ce37c9
unknown <pamelafugua@gmail.com>
2023-02-13 11:57:39 +0100

    Initial commit

---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+const Home = () => {
+    return(
+        <View>
+            <Text>Homey</Text>
+        </View>
+    )
+}
+
+export default Home;
